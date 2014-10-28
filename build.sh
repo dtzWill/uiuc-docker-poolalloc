380763a6c3c8b81b9b2fd0832e326108c4fafc64
Will Dietz <w@wdtz.org>
2014-10-28 12:23:52 -0500

    Use script to build 2nd stage, can't mount volumes with 'docker build'.

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+docker run --name llvm-git-build \
+  -i -t \
+  -v $PWD/llvm:/src/llvm \
+  -v $PWD/clang:/src/clang \
+  -v $PWD/compiler-rt:/src/compiler-rt \
+  -v $PWD/poolalloc:/src/poolalloc \
+  \
+  wdtz/llvm-test /bin/bash << EOF
+mkdir -p /llvm/build
+ln -sf /src/llvm /llvm/src
+ln -sf /src/clang /llvm/src/tools/
+ln -sf /src/compiler-rt /llvm/src/projects/
+ln -sf /src/poolalloc /llvm/src/projects/
+
+cd /llvm/build
+/llvm/src/configure --enable-optimized --prefix=/llvm/install
+make -j$(nproc)
+make check
+make check -C /llvm/build/projects/poolalloc
+make install
+EOF
+
+docker logs -f llvm-git-build
+docker wait llvm-git-build
+docker commit llvm-git-build wdtz/llvm-git
+docker rm llvm-git-build
